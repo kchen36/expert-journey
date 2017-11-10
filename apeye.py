67c69c0e47fa0feeff21004686d66302e388ca5a
Kerry Chen <kchen36@stuy.edu>
2017-11-10 14:47:59 -0500

    belikebill

--- apeye.py
+++ apeye.py
@@ -1,13 +1,14 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template
 import urllib2,json
 app = Flask(__name__)
 
 
 @app.route("/")
 def root():
-    u = urllib2.open("https://api.nasa.gov/planetary/apod?api_key=NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo")
+    
+    u = urllib2.urlopen("https://api.nasa.gov/planetary/apod?api_key=NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo")
     d = json.loads(u.read())
-    return render_template("index",data = d["date"],image=data["url"], copyr=data["copyright"],exp=data["explanation"])
+    return render_template("index.html",date = d["date"],image=d["url"], copyr=d["copyright"],exp=d["explanation"])
 if __name__ == "__main__":
     app.debug = True
     app.run()
